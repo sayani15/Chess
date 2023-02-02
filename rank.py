ed5542d8b42c83a7a9a4ac9f8af63a777c168e43
Sayani Pathak <sayanipathak01@gmail.com>
2023-02-02 19:50:28 +0000

    Add piece icons and continued ui.

--- rank.py
+++ rank.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class Rank(Enum):
-    pawn = 1
-    knight = 2
-    bishop = 3
-    rook = 4
-    queen = 5
-    king = 6
+    pawn = 1    # (1 material point)
+    knight = 2    # (3 material points)
+    bishop = 3    # (3 material points)
+    rook = 4    # (5 material points)
+    queen = 5    # (9 material points)
+    king = 6    # (no assigned value)
