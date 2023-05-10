6a41f6b4742437a102ea0b44ee0cc41c5662f414
Sayani Pathak <sayanipathak01@gmail.com>
2023-05-10 19:29:57 +0100

    All pieces changed to sprites

--- PieceSprite.py
+++ PieceSprite.py
@@ -1,9 +1,9 @@
 import pygame as pg
 
 class PieceSprite(pg.sprite.Sprite):
-    def __init__(self, position):
+    def __init__(self, position, image_file_path):
         super(PieceSprite, self).__init__()
-        self.image = pg.image.load("Pieces\\black\\pawn.png")
+        self.image = pg.image.load(image_file_path)
         self.rect = self.image.get_rect()
         self.rect.center = position
 
