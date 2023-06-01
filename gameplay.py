22061ce8ad5d153425aeabe1935a143471883011
Sayani Pathak <sayanipathak01@gmail.com>
2023-06-01 19:44:28 +0100

    Movement progress.

--- gameplay.py
+++ gameplay.py
@@ -1,6 +1,5 @@
 import pygame
 
-selected_sprite = None
 class ClickableSprite(pygame.sprite.Sprite):
 	def __init__(self, image_file_path, x, y, callback):
 		super().__init__()
@@ -12,27 +11,36 @@ class ClickableSprite(pygame.sprite.Sprite):
 		self.callback = callback
 		self.visible = True
 
-	def update(self, events):
-		for event in events:
-			if event.type == pygame.MOUSEBUTTONUP:
-				# if self.rect.collidepoint(event.pos):
-				# 	self.callback()
-				for sprite in group:
-					if sprite.rect.collidepoint(event.pos):
-						selected_sprite = sprite
-						if selected_sprite is not None:
-							self.callback()
-						break
+	
 
 	def movement(self, x: int, y: int):
 		self.rect.x = x
 		self.rect.y = y
 
 		return
+	
+	def update(self, events):
+		for event in events:
+			if event.type == pygame.MOUSEBUTTONUP:
+				for sprite in group:						
+					# selected_sprite = None
+					if sprite.rect.collidepoint(event.pos):					
+						selected_sprite = sprite	
+						break
+				try:
+					selected_sprite 
+				except NameError:
+					continue
+				if selected_sprite != None:					
+					clicked_pos_x = event.pos[0]
+					clicked_pos_y = event.pos[1]	
+					self.callback(selected_sprite, clicked_pos_x, clicked_pos_y)
+				
 
-def on_click():
 
-	selected_sprite.movement(100, 100)
+
+def on_click(selected_sprite, clicked_pos_x, clicked_pos_y):
+	selected_sprite.movement(clicked_pos_x, clicked_pos_y)
 	# a1_black_rook_sprite.visible = not a1_black_rook_sprite.visible
 
 
@@ -40,13 +48,13 @@ def on_click():
 pygame.init()
 screen = pygame.display.set_mode((570, 570))
 
-a1_black_rook_sprite = ClickableSprite("Pieces\\white\\rook.png", 31, 490, on_click)
+a1_white_rook_sprite = ClickableSprite("Pieces\\white\\rook.png", 31, 490, on_click)
 
-sprite = ClickableSprite("Pieces\\white\\bishop.png", 50, 50, on_click)
-h1_black_rook_sprite = ClickableSprite("Pieces\\white\\rook.png", 500, 490, on_click)
+white_bishop = ClickableSprite("Pieces\\white\\bishop.png", 50, 50, on_click)
+h1_white_rook_sprite = ClickableSprite("Pieces\\white\\rook.png", 500, 490, on_click)
 
 
-group = pygame.sprite.RenderPlain(a1_black_rook_sprite, sprite, h1_black_rook_sprite)
+group = pygame.sprite.RenderPlain(a1_white_rook_sprite, white_bishop, h1_white_rook_sprite)
 
 # group.add(a1_black_rook_sprite)
 # group.add(sprite)
@@ -61,7 +69,7 @@ while running:
 
 	group.update(events)
 	screen.blit(pygame.image.load("chessboard.png"), [0, 0])
-	if sprite.visible:
+	if white_bishop.visible:
 		group.draw(screen)
 	pygame.display.update()
 
